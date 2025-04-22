61316bd838f9e8cd544444675ceec840ea3dc437
Leonardo Andrey Pereira <leonhard84@outlook.com>
2025-04-22 08:43:36 -0300

    Include Contents

---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by Leonardo Andrey Pereira on 18/04/25.
+//
+
+import Foundation
---
+++ Reminder/Sources/Features/Constants/Text-Contants/Text-Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Text-Constants.swift
+//  Reminder
+//
+//  Created by Leonardo Andrey Pereira on 18/04/25.
+//
+
+import Foundation
