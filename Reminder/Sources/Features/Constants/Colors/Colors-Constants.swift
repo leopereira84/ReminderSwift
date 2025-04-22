60c594de606ad50ed5609c94280828f81d6e0b0e
Leonardo Andrey Pereira <leonhard84@outlook.com>
2025-04-22 08:43:36 -0300

    Include Contents

--- Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
+++ Reminder/Sources/Features/Constants/Colors/Colors-Constants.swift
@@ -2,7 +2,16 @@
 //  Colors-Constants.swift
 //  Reminder
 //
-//  Created by SETEC on 17/04/25.
+//  Created by Leonardo Pereira on 17/04/25.
 //
 
 import Foundation
+import UIKit
+
+public struct Colors {
+    static let primaryRedBase = UIColor(red: 192/255, green: 38/255, blue: 54/255, alpha: 1)
+    static let primaryBlueBase = UIColor(red: 51/255, green: 79/255, blue: 220/255, alpha: 1)
+    
+    static let secondaryRedBase = UIColor(red: 235/255, green: 60/255, blue: 77/255, alpha: 1)
+    static let secondaryBlueBase = UIColor(red: 110/255, green: 131/255, blue: 236/255, alpha: 1)
+}
